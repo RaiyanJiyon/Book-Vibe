fe8a99db2fd9f9144ece2c777e0d9291c1c37655
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-10 11:31:29 +0600

    Added window scroll useeffect for better user experience

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import { addToReadList, addToWishList } from '../../utility/addToDB';
 
 const BookDetails = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
     const params = useParams();
     const { bookId } = params;
     const id = parseInt(bookId);
