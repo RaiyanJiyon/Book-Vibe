bab35073a0ee121076b02b3e5d2b4436334ace8f
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-10 11:38:34 +0600

    Made View The List button redirect to ListedBook page after clicking

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import bannerImage from '../../assets/books.jpg'
+import { NavLink } from 'react-router-dom';
 
 const Banner = () => {
     return (
@@ -10,7 +11,7 @@ const Banner = () => {
                     className="w-80 rounded-lg shadow-2xl" />
                 <div>
                     <h1 className="text-5xl text-center lg:text-left font-bold">Books to freshen up your bookshelf</h1>
-                    <button className="btn bg-[#23BE0A] text-white font-semibold mt-10">View The List</button>
+                    <NavLink to={"/ListedBooks"} className="btn bg-[#23BE0A] text-white font-semibold mt-10">View The List</NavLink>
                 </div>
             </div>
         </div>
